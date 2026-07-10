6f2327ea4383ed6d1999eff0a8430aae799aff41
Harsha0Sree <harshaskavish@gmail.com>
2026-07-10 07:54:40 -0700

    define the database schema

---
+++ app/models/schemas.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Document(Base):
+    __tablename__ = "documents"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    filename: Mapped[str] = mapped_column(unique=True)
+    bucket: Mapped[str]
+    object_key: Mapped[str]
+    status: Mapped[str]
+    size: Mapped[int]
+    created_at: Mapped[datetime]
