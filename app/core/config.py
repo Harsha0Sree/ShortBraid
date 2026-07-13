72d6d332767f6d32459a4b7912d89946b59fec6e
Harsha0Sree <harshaskavish@gmail.com>
2026-07-13 02:46:01 -0700

    add settings

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     minio_access_key: str
     minio_secret_key: str
     bucket_name: str
+    database_url: str
     model_config = SettingsConfigDict(env_file=".env")
 
 
